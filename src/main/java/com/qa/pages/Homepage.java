8e1ced1d1e7d2028cf0d31f319bb379c2c59a593
Sabin <sabinsuwal@gmail.com>
2022-08-29 13:10:00 -0400

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class Homepage {
+	
+	public void HomePage() {
+		System.out.println("Home Page");
+	}
+}
---
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class Loginpage {
+
+	public void LoginPage() {
+		System.out.println("Login Page");
+	}
+}
